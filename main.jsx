b843d0f89b72443627d6459b7538ca906e4a6e2b
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-08-06 21:43:31 -0500

    contniar con el proyecto

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
-import { App } from "./src/App.jsx";
+import { App } from './src/App.jsx'
 import './style.css'
 
 const root = createRoot(document.getElementById('root'))
