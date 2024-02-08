95ab9f96bc2186e9550bcd961549d646727b6913
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-08 09:28:31 -0500

    peticion fecht ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsService } from './api/products.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app';
+  private readonly productsSvc = inject(ProductsService);
+  products$ = this.productsSvc.getAllProducts();
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [provideHttpClient(withFetch())]
 };
