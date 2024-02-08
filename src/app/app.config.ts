2dac9ca89d71a7229efe8a72e97d3268e47fa38b
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-08 09:39:13 -0500

    interceptors para errores

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
+import { ErrorResponseInterceptor } from './shared/error-response.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withFetch())]
+  providers: [provideHttpClient(withFetch(), withInterceptors([ErrorResponseInterceptor]))]
 };
---
+++ src/app/shared/error-response.interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
+import { catchError, throwError } from "rxjs";
+
+export const ErrorResponseInterceptor: HttpInterceptorFn = (req, next) =>
+  next(req).pipe(catchError(handleErrorResponse))
+
+function handleErrorResponse(error: HttpErrorResponse) {
+  const errorResponse = `Error Status: ${error.status}, message: ${error.message}`;
+  return throwError(() => errorResponse)
+}
